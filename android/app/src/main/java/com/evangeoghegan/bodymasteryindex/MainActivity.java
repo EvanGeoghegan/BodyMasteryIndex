403eba4327eae16a8f5aec5435dc7d10ffd96b60
Evan Geoghegan <evangeoghegan@evans-air.home>
2025-06-22 12:42:35 +0100

    Initial commit

---
+++ android/app/src/main/java/com/evangeoghegan/bodymasteryindex/MainActivity.java
@@ -0,0 +1,5 @@
+package com.evangeoghegan.bodymasteryindex;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.evangeoghegan.bodymasteryindex',
+  appName: 'BodyMasteryIndex',
+  webDir: 'dist/public'
+};
+
+export default config;
